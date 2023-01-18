e2173023444f9214538ef1cfd01201e4f9efaac4
Rowan <Olsonrowan@gmail.com>
2023-01-17 23:53:01 -0700

    deleted commented code

--- index.js
+++ index.js
@@ -17,17 +17,9 @@ exports.handler = (event, context) => {
         Key: "rowanolson/results.txt"
     }
     s3.putObject(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-        /*
-        data = {
-         ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
-         VersionId: "tpf3zF08nBplQK1XLOefGskR7mGDwcDk"
-        }
-        */
+        if (err) console.log(err, err.stack);
+        else     console.log(data);           
+       
       });
 
-    // bucket/yourname/results.txt
-
-    // Return the string
 };
